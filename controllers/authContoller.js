7e03540e8bf893366fa4c68233c5c3f6b0b3ff30
hiDeniz <halilibrahimdeniz2001@gmail.com>
2024-05-03 01:26:30 +0300

    4 digit otp

--- controllers/authContoller.js
+++ controllers/authContoller.js
@@ -5,7 +5,7 @@ const { sendEmail } = require('../utils/mailer');
 
 // Function to generate a random verification code
 const generateVerificationCode = () => {
-    return Math.floor(100000 + Math.random() * 900000).toString();
+    return Math.floor(1000 + Math.random() * 9000).toString();
 };
 
 module.exports = {
