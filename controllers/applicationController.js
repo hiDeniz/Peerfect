20eaf5533bc9269c26c221448c0ee67670aa30bc
hiDeniz <halilibrahimdeniz2001@gmail.com>
2024-06-01 17:11:42 +0300

    application services

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -33,5 +33,55 @@ module.exports = {
         } catch (error) {
             res.status(500).json(error);
         }
-    }
+    },
+
+    // Update Application func
+    updateApplication: async (req, res) => {
+        try {
+            const updatedApplication = await Application.findByIdAndUpdate(
+                req.params.id,
+                {$set: req.body},
+                {new: true}
+            );
+            const {__v, createdAt, updatedAt, ...updatedApplicationInfo} = updatedApplication._doc;
+            res.status(200).json(updatedApplicationInfo);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+    // Delete Application func
+    deleteApplication: async (req, res) => {
+        try {
+            await Application.findByIdAndDelete(req.params.id)
+            res.status(200).json("Application Successfully Deleted");
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+    // Get Application func
+    getApplication: async (req, res) => {
+        try {
+            const application = await Application.findById(req.params.id)
+                .populate({
+                    path: 'owner',
+                    select: '_id name surname imageUrl'
+                });
+
+            res.status(200).json(application);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+    // Get All Application func
+    getAllApplications: async (req, res) => {
+        try {
+            const application = await Application.find()
+            res.status(200).json(application);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
 }
--- routes/application.js
+++ routes/application.js
@@ -5,4 +5,16 @@ const {verifyToken} = require("../middleware/verifyToken");
 // POST APPLICATION
 router.post("/", verifyToken, applicationController.createApplication);
 
+// UPDATE APPLICATION
+router.put("/:id", verifyToken, applicationController.updateApplication);
+
+// DELETE PROJECT
+router.delete("/:id", verifyToken, applicationController.deleteApplication);
+
+// GET PROJECT
+router.get("/:id", verifyToken, applicationController.getApplication);
+
+// GET ALL PROJECT
+router.get("/", verifyToken, applicationController.getAllApplications);
+
 module.exports = router
